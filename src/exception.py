047834af0bf831e8876db341af2070168a8e9d67
Soham Nemade <nemadesoham21@gmail.com>
2026-03-15 19:35:53 +0530

    exception and logging

---
+++ src/exception.py
@@ -0,0 +1,22 @@
+import sys
+import logging
+
+logging.basicConfig(level=logging.ERROR)
+
+
+def error_message_details(error,error_detail:sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = f"Error occurred in script: {file_name} at line number: {line_number} with error message: {str(error)}"
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_details(error_message,error_details=error_details)
+
+    def __str__(self):
+        return self.error_message
+    
+    
