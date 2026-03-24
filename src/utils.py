928cb7b5822defd0532337acc0e80cacdc69f63f
Soham Nemade <nemadesoham21@gmail.com>
2026-03-24 18:39:39 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import dill
 from sklearn.metrics import r2_score
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -19,30 +20,22 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try:
         report={}
         for i in range(len(models)):
+             
+
             model=list(models.values())[i]
+            para=params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            y_test_pred=model.predict(X_test)
-
-            test_model_score=r2_score(y_test,y_test_pred)
-
-            report[list(models.keys())[i]]=test_model_score
-
-        return report
-
-    except Exception as e:
-        raise CustomException(e,sys)
-    
-def evaluate_model(X_train,y_train,X_test,y_test,models):
-    try:
-        report={}
-        for i in range(len(models)):
-            model=list(models.values())[i]
-            model.fit(X_train,y_train)
+           # model.fit(X_train,y_train)
 
             y_test_pred=model.predict(X_test)
 
